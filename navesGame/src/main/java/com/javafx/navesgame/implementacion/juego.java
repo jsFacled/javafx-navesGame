c91119bca4da8b2a937c8607a0e97159ca16ec58
faGitLab <facled@yahoo.com.ar>
2024-01-04 10:06:35 -0300

    acomodando el codigo para comenzar

--- navesGame/src/main/java/com/javafx/navesgame/implementacion/juego.java
+++ navesGame/src/main/java/com/javafx/navesgame/implementacion/juego.java
@@ -9,26 +9,62 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
 
 public class juego extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
 
+//----variables globales ---------
 
+    GraphicsContext graficos;
+    Group root;
+    Scene scene;
+    Canvas lienzo;
+
+//------start------------
+    /*
+    1-inicializa componentes
+    2-pinta
+    3-asigna la ventana al stage
+    4-agrega titulo
+    5-muestra la ventana
+     */
+//------------------------
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/javafx/navesgame/juego-view.fxml"));
-        Group root = new Group();
         //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        Scene scene = new Scene(root, 700,500);
-        Canvas lienzo = new Canvas(200,200);
-        root.getChildren().add(lienzo);
-
-        GraphicsContext graficos = lienzo.getGraphicsContext2D();//Dentro de graficos ya puedo pintar o dibujar
-       graficos.fillRect(20, 50, 25, 68);//pos x, pos y, ancho, alto
+        inicializarComponentes();
+        pintar();
         stage.setScene(scene);
         stage.setTitle("JavaFx-Game!");
+
+
         stage.show();
+    }
+
+
+
+/*
+    ----------- Inicializar componentes ------------------
+ */
+
+    public void inicializarComponentes(){
+        root = new Group();
+        scene = new Scene(root, 700,500);
+        lienzo = new Canvas(200,200);
+        root.getChildren().add(lienzo);
+        graficos = lienzo.getGraphicsContext2D();//Dentro de graficos ya puedo pintar o dibujar
 
     }
 
-    public static void main(String[] args) {
-        launch();
+/*
+    ----------- Pintar ------------------
+ */
+
+    public void pintar(){
+        graficos.fillRect(20, 50, 25, 68);//pos x, pos y, ancho, alto
     }
+
+
+//fin
 }
