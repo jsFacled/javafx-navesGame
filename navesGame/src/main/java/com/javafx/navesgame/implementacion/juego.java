297669a7d5a63a6c0bb599f42aca3c6d59739c91
faGitLab <facled@yahoo.com.ar>
2024-01-04 09:50:02 -0300

    lienzo y grafico construidos

--- navesGame/src/main/java/com/javafx/navesgame/implementacion/juego.java
+++ navesGame/src/main/java/com/javafx/navesgame/implementacion/juego.java
@@ -2,7 +2,10 @@ package com.javafx.navesgame.implementacion;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
 
 public class juego extends Application {
@@ -11,9 +14,16 @@ public class juego extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/javafx/navesgame/juego-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("JavaFx-Game!");
+        Group root = new Group();
+        //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(root, 700,500);
+        Canvas lienzo = new Canvas(200,200);
+        root.getChildren().add(lienzo);
+
+        GraphicsContext graficos = lienzo.getGraphicsContext2D();//Dentro de graficos ya puedo pintar o dibujar
+       graficos.fillRect(20, 50, 25, 68);//pos x, pos y, ancho, alto
         stage.setScene(scene);
+        stage.setTitle("JavaFx-Game!");
         stage.show();
 
     }
